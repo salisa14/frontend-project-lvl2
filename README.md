3d9aebe98d8f807eb89ab193d2b83542317a83e2
Salisa <salisund@gmail.com>
2022-04-10 16:47:07 +0300

    add command

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/salisa14/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/salisa14/frontend-project-lvl2/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/8627568d20f153af078d/maintainability)](https://codeclimate.com/github/salisa14/frontend-project-lvl2/maintainability)
---
+++ bin/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { gendiff } from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  });
+
+program.parse(process.argv);
